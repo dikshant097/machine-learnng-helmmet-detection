461cdaf6035bae29c0f81908c914dde1ec690157
Dikshant Manocha <manocha.dikshant@gmail.com>
2018-11-25 14:10:36 +0530

    Initial Commit

---
+++ final/HelmetDetection.java
@@ -0,0 +1,63 @@
+import org.opencv.core.Core; 
+import org.opencv.core.Mat; 
+import org.opencv.core.MatOfRect; 
+import org.opencv.core.Point; 
+import org.opencv.core.Rect; 
+import org.opencv.core.Scalar; 
+import org.opencv.imgcodecs.Imgcodecs; 
+import org.opencv.imgproc.Imgproc; 
+import org.opencv.objdetect.CascadeClassifier; 
+  
+ class HelmetDetection 
+{ 
+    public static void main(String[] args) 
+    {  
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);  
+        CascadeClassifier helmetDetector = new CascadeClassifier(); 
+        helmetDetector.load("helmet.xml"); 
+  
+        // Input image 
+        Mat image = Imgcodecs.imread("E:\\Major\\final\\input\\1.jpg"); 
+  
+        // Detecting Helemt 
+        MatOfRect helmet = new MatOfRect(); 
+        helmetDetector.detectMultiScale(image, helmet); 
+  
+        // Creating a rectangular box showing faces detected 
+		
+		if(helmet.toArray().length!=0)
+		{
+			 
+			//String filename = "Ouput.jpg"; 
+			//Imgcodecs.imwrite("E:\\Major\\final\\output\\"+filename, image);
+				
+			CascadeClassifier numberPlate = new CascadeClassifier(); 
+			numberPlate.load("numberplate.xml");
+
+			Mat numberPlateImage = Imgcodecs.imread("E:\\Major\\final\\output\\Output.jpg"); 
+			
+			MatOfRect number=new MatOfRect();
+			numberPlate.detectMultiScale(image, number);
+			
+			
+			 for (Rect rect : number.toArray()) 
+			{
+				System.out.println("number plate");
+				Imgproc.rectangle(image, new Point(rect.x, rect.y), 
+				new Point(rect.x + rect.width, rect.y + rect.height), 
+                                          new Scalar(0, 255, 0));
+			}
+			for (Rect rect : helmet.toArray()) 
+			{ 
+				System.out.println("No Helmet");
+				Imgproc.rectangle(image, new Point(rect.x, rect.y), 
+				new Point(rect.x + rect.width, rect.y + rect.height), 
+                                           new Scalar(0, 255, 0)); 
+			}
+			String filename="output.jpg";	
+			Imgcodecs.imwrite("E:\\Major\\final\\output\\"+filename, image); 
+			
+		} 
+        
+    } 
+} 
